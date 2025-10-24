554268da818b7317dcb1b3fc9ce8188a903c8234
austingomijr <austingomijr@gmail.com>
2025-10-24 05:33:32 +0200

    Add files via upload

---
+++ a.js
@@ -0,0 +1,43 @@
+JESUS
+
+
+const q = [{
+  "qq": " HAS FACE AND HANDS",
+  "as": [{
+    "asq":"watc",
+    "true":"true"
+    
+  },
+  {
+    "asq":"qqqq",
+    "true":"false"
+  }]
+}]
+
+let sor = 0
+let crsor = 0
+let i = false
+
+
+const qb = document.getElementById("q")
+
+const as = document.querySelectorAll("as-1")
+
+const asx = document.getElementById("as")
+
+function s(){
+  let i = true
+  asx.innerHtml="st"
+  t()
+}
+
+function t(){
+  for(i=0;i<q.length;i++){
+    qr = q[i].qq
+    console.log(qr)
+    qb.innerHtml=qr
+    for(i=0;i<as.length;i++){
+      const asb =as[i]
+      asb.innerHtml= q[i].as.i.asq
+  }}
+}
